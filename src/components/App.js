3e0393fa27013b8ded1713b4f0fe3716def360c7
Kr-Krishan <krishanilam@gmail.com>
2020-07-11 00:26:52 +0530

    added Private Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import { fetchPosts } from '../actions/posts';
 import { Navbar, Home, Page404, Login, Signup } from './index';
 import propTypes from 'prop-types';
@@ -12,6 +17,21 @@ import { authenticateUser } from '../actions/auth';
 //   return <h2>Signup</h2>;
 // };
 
+const Settings = () => {
+  return <div>Settings</div>;
+};
+
+const PrivateRoute = (privateRouteProps) => {
+  const { path, isLoggedIn, component: Component } = privateRouteProps;
+  return (
+    <Route
+      path={path}
+      render={(props) => {
+        return isLoggedIn ? <Component {...props} /> : <Redirect to="/login" />;
+      }}
+    />
+  );
+};
 class App extends React.Component {
   // to fetch all the posts from api
   componentDidMount() {
@@ -34,7 +54,7 @@ class App extends React.Component {
 
   render() {
     // console.log('props', this.props);
-    const { posts } = this.props;
+    const { posts, auth } = this.props;
     return (
       <Router>
         <div>
@@ -52,6 +72,11 @@ class App extends React.Component {
           />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
+          <PrivateRoute
+            path="/settings"
+            component={Settings}
+            isLoggedIn={auth.isLoggedIn}
+          />
           <Route component={Page404} />
         </Switch>
       </Router>
@@ -63,6 +88,7 @@ class App extends React.Component {
 function mapStateToProps(state) {
   return {
     posts: state.posts,
+    auth: state.auth,
   };
 }
 
