bc7aa96aa39fb6ca658aea7c3583d7b4451bda7f
Kr-Krishan <krishanilam@gmail.com>
2020-08-09 11:37:16 +0530

    Updates

---
+++ precache-manifest.036ef2b10c89027bbaaeeb9260ab23d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddb5111fc95f24e5448a2c607fffc38",
+    "url": "/codeial/index.html"
+  },
+  {
+    "revision": "8ce801758b565bd1d20f",
+    "url": "/codeial/static/css/main.d46292b8.chunk.css"
+  },
+  {
+    "revision": "c18850d1058c4fdd4058",
+    "url": "/codeial/static/js/2.00eade24.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/codeial/static/js/2.00eade24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce801758b565bd1d20f",
+    "url": "/codeial/static/js/main.7f6ce7d4.chunk.js"
+  },
+  {
+    "revision": "4d6c33b4a8408b7ee474",
+    "url": "/codeial/static/js/runtime-main.48c318f0.js"
+  }
+]);
